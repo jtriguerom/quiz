b209c85610bad8fa3cf96f585419356f27de0e26
jtriguerom <joseantoniotriguero@gmail.com>
2015-08-01 19:22:10 +0200

    Despliegue DB en heroku

--- models/models.js
+++ models/models.js
@@ -1,15 +1,34 @@
 var path = require('path');
 
+// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+// SQLite DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name   = (url[6]||null);
+var user      = (url[2]||null);
+var pwd       = (url[3]||null);
+var protocol  = (url[1]||null);
+var dialect   = (url[1]||null);
+var port      = (url[5]||null);
+var host      = (url[4]||null);
+var storage   = process.env.DATABASE_STORAGE;
+
 //cargar modelo ORM
 var Sequelize = require('sequelize');
 
-//usar bbdd Sqlite
-var sequelize = new Sequelize(null, null, null,
-							{dialect: "sqlite", storage: "quiz.sqlite"}
-						);
+//usar bbdd SQLite o Postgres
+var sequelize = new Sequelize(DB_name, user, pwd,
+	{ dialect : protocol,
+		protocol: protocol,
+		port:     port,
+		host:     host,
+		storage:  storage,   // solo SQLite (.env)
+		omitNull: true       // solo Postgres
+	}
+);
 
-//Importar la definicion de la tabla Quiz en quiz.js
-var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+//Importar la definicion de la tabla Quiz en quiz.js en dos pasos
+var quiz_path = path.join(__dirname, 'quiz')
+var Quiz = sequelize.import(quiz_path);
 
 exports.Quiz = Quiz;//exportar definicion de la tabla quiz
 
