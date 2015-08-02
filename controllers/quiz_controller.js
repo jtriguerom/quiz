5091df832453f5ec9e9af9a452185a97d20345a5
jtriguerom <joseantoniotriguero@gmail.com>
2015-08-02 20:39:37 +0200

    Modificaciones video 1 tema 8

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -49,6 +49,24 @@ exports.answer = function(req, res) {
 	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 };
 
+// GET /quizes/new
+exports.new = function(req, res){
+  var quiz = models.Quiz.build( //crea objeto quiz
+    {pregunta: 'Pregunta', respuesta: 'Respuesta'}
+  );
+  res.render('quizes/new', {quiz: quiz});
+};
+
+// GET /quizes/create
+exports.create = function(req, res){
+  var quiz = models.Quiz.build(req.body.quiz);
+
+  // guarda en DB los campos preguntas y respuestas de quiz
+  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+      res.redirect('/quizes');
+  })
+};
+
 //GET /author
 exports.author = function(req, res) {
 	res.render('author', {author: 'Jose Antonio Triguero'});
